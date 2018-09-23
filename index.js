25dad6a92be9e476adb95e83e835d9a8c8bfadca
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-09-22 20:08:13 -0400

    Add scaffolding for a command line interface

--- index.js
+++ index.js
@@ -4,11 +4,17 @@
  */
 
 const server = require('./lib/server');
+const cli = require('./lib/cli');
 
 const app = {};
 
 app.init = () => {
     server.init();
+
+    //start the cli last
+    setTimeout(() => {
+        cli.init();
+    }, 100);
 };
 
 //Start the app
